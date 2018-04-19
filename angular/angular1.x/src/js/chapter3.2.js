b004ac363022b94371c25fc363559d9e85a7fdf0
yanghj7425 <1501587673@qq.com>
2018-04-20 07:06:02 +0800

    add ng-options practice

--- angular/angular1.x/src/js/chapter3.2.js
+++ angular/angular1.x/src/js/chapter3.2.js
@@ -1,1 +1,19 @@
-var app = angular.module
+var app = angular.module('myApp',[])
+app.controller('myAppController',function($scope){
+	$scope.text = 'htl';
+	$scope.fruit = [
+		{ label: "Apples", value: 1, id: 2 },
+		{ label: "Oranges", value: 2, id: 1 },
+		{ label: "Limes", value: 4, id: 4 },
+		{ label: "Lemons", value: 5, id: 3 }];
+		$scope.selectedFruitNgOptions = '';
+		$scope.num = 2;
+
+	$scope.change = function(selectedFruitNgOptions){
+		if ((selectedFruitNgOptions || '') == ''){
+			return false;
+		}
+		console.log(selectedFruitNgOptions);
+		return true;
+	}
+})
